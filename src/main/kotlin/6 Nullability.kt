40cd84157f97ce8a9d98b9b8d10f07eadc3b603b
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2023-11-06 08:21:46 +0100

    Updates 6 Nullability.kt.

--- src/main/kotlin/6 Nullability.kt
+++ src/main/kotlin/6 Nullability.kt
@@ -9,9 +9,10 @@ fun main() {
     val name: String = "Alice"
     // TODO make the two below lines compile
     // name = null
-    // println(name.length)
+    // println(name.length) // null
 
 
     val randomPositiveNumber = getRandomPositiveNumber()
     val randomNumber: Int // TODO -1 if null, otherwise the number itself (Elvis)
+    // println(randomNumber)
 }
