2234bdbcc5b5f89449669d497726bb8d1bada6e4
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2023-11-06 08:21:23 +0100

    Updates 4 Functions.kt.

--- src/main/kotlin/4 Functions.kt
+++ src/main/kotlin/4 Functions.kt
@@ -1,13 +1,16 @@
 fun main() {
-    val greeting = birthdayGreeting(name = "Alice", age = 27)
+    val greeting = birthdayGreeting(name = "Alice", age = 42)
     println(greeting)
 
-    // TODO make the below line compile with the instructions below
-    // val greetingWithName = birthdayGreeting(name = "Alice", age = 27, author = "Bob")
+    // TODO make the two lines below compile with the instructions below
+    // val greetingWithName = birthdayGreeting(name = "Alice", author = "Bob")
     // println(greetingWithName)
+    // Output:
+    // Happy Birthday, Alice!
+    // You're now 42 years old!
+    // Greets Bob!
 }
 
-// TODO Tutorial
 // TODO adjust this function to have 42 as the default parameter for age
 // TODO add a parameter "author" with an empty string as a parameter
 // TODO use "author" to add a greet: "Greets <author>!"
